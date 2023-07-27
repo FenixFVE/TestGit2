04ab6d193f0fa52de456429699073f3d1ec662b9
FenixFVE <49178401+FenixFVE@users.noreply.github.com>
2023-07-27 04:22:04 +0000

    AddFunction

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static int AddFunction(int a, int b)
+    {
+        return a + b;
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, World and {AddFunction(2, 2)}!");
+    }
+}
